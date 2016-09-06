4fb38d258b63f7535929beccfd9fc060ff48794e
feizheng <1290657123@qq.com>
2016-09-06 10:23:45 +0800

    update swiper pager

--- DropCooperation/views/home-content/index.js
+++ DropCooperation/views/home-content/index.js
@@ -12,30 +12,22 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
-
+import Swiper from 'react-native-swiper';
 export default class HomeContent extends Component {
   render(){
     return (
       <View style={styles.container}>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
-        <Text>Home!</Text>
+        <Swiper style={styles.wrapper} height={120}>
+          <View style={styles.slide1}>
+            <Text style={styles.text}>Hello Swiper</Text>
+          </View>
+          <View style={styles.slide2}>
+            <Text style={styles.text}>Beautiful</Text>
+          </View>
+          <View style={styles.slide3}>
+            <Text style={styles.text}>And simple</Text>
+          </View>
+        </Swiper>
       </View>
     );
   }
--- DropCooperation/views/home-content/styles.js
+++ DropCooperation/views/home-content/styles.js
@@ -5,11 +5,37 @@ import {
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   f20:{
     fontSize:20
+  },
+  wrapper:{
+    top:64
+  },
+  slide1: {
+    flex: 1,
+    height:100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    height:100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
   }
 });
