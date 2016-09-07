a0bfaa09599f13a73003232ec9296b2674ceac77
feizheng <1290657123@qq.com>
2016-09-07 11:14:46 +0800

    complete swiper

--- DropCooperation/views/home-content/index.js
+++ DropCooperation/views/home-content/index.js
@@ -9,25 +9,28 @@ import {
   AppRegistry,
   Text,
   View,
+  AlertIOS,
+  Image,
   ScrollView
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import bannerData from '../../assets/apis/banner-list.json';
 import styles from './styles';
 import Swiper from 'react-native-swiper';
 export default class HomeContent extends Component {
   render(){
+    var slides = [];
+    bannerData.data.forEach(function(item,index) {
+      slides.push(
+        <View style={styles.slide} key={index}>
+          <Image style={{ width:375,height:133}} source={{uri: item.imgUrl}}/>
+        </View>
+      );
+    })
     return (
       <ScrollView style={styles.container}>
-        <Swiper style={styles.wrapper} height={140}>
-          <View style={styles.slide1}>
-            <Text style={styles.text}>Hello Swiper</Text>
-          </View>
-          <View style={styles.slide2}>
-            <Text style={styles.text}>Beautiful</Text>
-          </View>
-          <View style={styles.slide3}>
-            <Text style={styles.text}>And simple</Text>
-          </View>
+        <Swiper style={styles.wrapper} height={133}>
+          {slides}
         </Swiper>
       </ScrollView>
     );
--- DropCooperation/views/home-content/styles.js
+++ DropCooperation/views/home-content/styles.js
@@ -12,23 +12,11 @@ export default StyleSheet.create({
   },
   wrapper:{
   },
-  slide1: {
+  slide: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#9DD6EB',
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5',
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9',
   },
   text: {
     color: '#fff',
